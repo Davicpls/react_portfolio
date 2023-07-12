8bca2c19608c59afe615fcfef37e607bbac02ac1
Davi Coelho <davic2stl@outlook.com>
2023-07-11 23:19:39 -0300

    pegando dados do git para montar um box/tabela...

--- src/App.js
+++ src/App.js
@@ -1,59 +1,100 @@
-import React from 'react';
-import './App.css';
-import { Box, Button, Divider } from '@mui/material/';
-import TabsPanel from './Components/Tabs/TabPanel';
-import TabsPanelPt from './Components/Tabs/TabPanelPt';
-import { useState } from 'react'
-import bandeirabr from './Components/Images/bandeirabr.jpeg'
-import bandeirauk from './Components/Images/bandeirauk.jpg'
-
-
-
+import React from "react";
+import "./App.css";
+import { Box, Button } from "@mui/material/";
+import TabsPanel from "./Components/Tabs/TabPanel";
+import TabsPanelPt from "./Components/Tabs/TabPanelPt";
+import { useState } from "react";
+import bandeirabr from "./Components/Images/bandeirabr.jpeg";
+import bandeirauk from "./Components/Images/bandeirauk.jpg";
+import octokit from "./Hooks/useOctokit";
+import { parseISO } from "date-fns";
 
 function App() {
+  async function getUserData() {
+    const response = await octokit.request(
+      "GET /repos/Davicpls/react_portfolio/commits"
+    );
+    let multiDateTime = []
+      response.data.map(e => {
+      multiDateTime.push(e["commit"]["author"]["date"])
+      return multiDateTime
+    })
+    multiDateTime.forEach((date, index, array) => {
+        array[index] = parseISO(date)
+    })
+
+    return multiDateTime;
+  }
 
 
-const [changeLanguage, setChangeLanguage] = useState('ptbr')
+  getUserData()
+    .then((res) => {
+      console.log(res)
+    })
 
+    .catch((err) => {
+      console.log(err);
+    });
 
-const handleChangeLanguage = (newValue) => {
-    setChangeLanguage(newValue)
-
-}
-
-
+  const [changeLanguage, setChangeLanguage] = useState("ptbr");
+  const handleChangeLanguage = (newValue) => {
+    setChangeLanguage(newValue);
+  };
 
   return (
-    <Box sx={{width:'1920px'}}>
-      <div className='NavBar'>
-        <Box sx={{width:'39%'}} display={'flex'} justifyContent={'end'}  alignItems={'center'}>
+    <Box sx={{ width: "1920px" }}>
+      <div className="NavBar">
+        <Box
+          sx={{ width: "39%" }}
+          display={"flex"}
+          justifyContent={"end"}
+          alignItems={"center"}
+        >
           Portfolio
         </Box>
-        
-          <Box sx={{width:'30%', fontSize:'18px'}} justifyContent={'end'} display={'flex'} alignItems={'center'}>
-          
-          {changeLanguage === 'ptbr' ? <>Idioma:</> : <>Language:</>}
-            <Box sx={{paddingLeft:'1vh'}} display={'flex'} alignItems={'center'}>
-            <Button onClick={() => handleChangeLanguage('ptbr')} >
-            <img width={'80%'} height={'25px'} src={bandeirabr} alt="br_flag" className='flagImgs'></img>
+
+        <Box
+          sx={{ width: "30%", fontSize: "18px" }}
+          justifyContent={"end"}
+          display={"flex"}
+          alignItems={"center"}
+        >
+          {changeLanguage === "ptbr" ? <>Idioma:</> : <>Language:</>}
+          <Box
+            sx={{ paddingLeft: "1vh" }}
+            display={"flex"}
+            alignItems={"center"}
+          >
+            <Button onClick={() => handleChangeLanguage("ptbr")}>
+              <img
+                width={"80%"}
+                height={"25px"}
+                src={bandeirabr}
+                alt="br_flag"
+                className="flagImgs"
+              ></img>
             </Button>
-            <Button onClick={() => handleChangeLanguage('en')}>
-            <img width={'80%'} height={'25px'} src={bandeirauk} alt="uk_flag" className='flagImgs'></img>
+            <Button onClick={() => handleChangeLanguage("en")}>
+              <img
+                width={"80%"}
+                height={"25px"}
+                src={bandeirauk}
+                alt="uk_flag"
+                className="flagImgs"
+              ></img>
             </Button>
-            </Box>
           </Box>
-          </div>
-      <div className='Middle'>
-        <Box sx={{width:'100%'}} display={'flex'} justifyContent={'center'}>
-        {changeLanguage === 'ptbr' ?
-        <TabsPanelPt>
-        </TabsPanelPt>
-          :
-          <TabsPanel>
-          </TabsPanel>
-          }
-        </Box> 
-        </div>
+        </Box>
+      </div>
+      <div className="Middle">
+        <Box sx={{ width: "100%" }} display={"flex"} justifyContent={"center"}>
+          {changeLanguage === "ptbr" ? (
+            <TabsPanelPt></TabsPanelPt>
+          ) : (
+            <TabsPanel></TabsPanel>
+          )}
+        </Box>
+      </div>
     </Box>
   );
 }
