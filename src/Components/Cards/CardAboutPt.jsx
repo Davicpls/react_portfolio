755407284812b5261595e133ccb90f55be012d4b
Davi Coelho <davic2stl@outlook.com>
2023-07-27 22:57:06 -0300

    cleaning dependecies

--- src/Components/Cards/CardAboutPt.jsx
+++ src/Components/Cards/CardAboutPt.jsx
@@ -1,14 +1,10 @@
 import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import Typography from "@mui/material/Typography";
 import {
-  Button,
-  CardActionArea,
-  CardActions,
   List,
   ListItem,
   Collapse,
