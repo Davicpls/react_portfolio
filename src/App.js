c548cd7752abc8214d7d0a605f0b41939cd030f3
Davi Coelho <davic2stl@outlook.com>
2023-07-30 00:15:44 -0300

    tentando responsividade

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import mui from "../src/Components/Icons/mui.png";
 import DarkModeIcon from "@mui/icons-material/DarkMode"; */
 import GitHubIcon from "@mui/icons-material/GitHub";
 import CircleIcon from "@mui/icons-material/Circle";
-import { Box, List, ListItem, IconButton, Paper } from "@mui/material/";
+import { List, ListItem, IconButton, Paper } from "@mui/material/";
 import TabsPanel from "./Components/Tabs/TabPanel";
 import TabsPanelPt from "./Components/Tabs/TabPanelPt";
 import { useState } from "react";
@@ -14,6 +14,7 @@ import bandeirabr from "./Components/Images/bandeirabr.jpeg";
 import bandeirauk from "./Components/Images/bandeirauk.jpg";
 import octokit from "./Hooks/useOctokit";
 import { parseISO, format } from "date-fns";
+import { Box, AppBar, Toolbar, Typography, Container } from "@mui/material";
 
 function App() {
   const reactDocs = () => {
@@ -59,12 +60,6 @@ function App() {
     setChangeLanguage(newValue);
   };
 
-  /*   const [switchTheme, setSwitchTheme] = useState('light')
-
-  const handleChangeTheme = (newValue) => {
-    setSwitchTheme(newValue)
-  } */
-
   if (isLoading) {
     return <div className="initialLoad">Loading...</div>;
   }
@@ -82,169 +77,122 @@ function App() {
       sx={{
         display: "flex",
         flexDirection: "column",
-        justifyContent: "center",
-        width: "1920px",
+        minHeight: "100vh"
       }}
     >
-      <Paper sx={{ mb: "auto" }} elevation={5}>
-        <Box className="NavBar">
-          <Box
-            sx={{ width: "39%", marginRight: "30px" }}
-            display={"flex"}
-            justifyContent={"end"}
-            alignItems={"center"}
-          >
-            Portfolio
-          </Box>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Cabeçalho</Typography>
+        </Toolbar>
+      </AppBar>
 
-          <Box
-            sx={{ width: "30%", fontSize: "18px" }}
-            justifyContent={"end"}
-            display={"flex"}
-            alignItems={"center"}
-          >
-            {changeLanguage === "ptbr" ? <>Language:</> : <>Idioma:</>}
-            <Box
-              sx={{ paddingLeft: "1vh" }}
-              display={"flex"}
-              alignItems={"center"}
-            >
-              <IconButton onClick={() => handleChangeLanguage("ptbr")}>
-                <img
-                  width={"80%"}
-                  height={"25px"}
-                  src={bandeirabr}
-                  alt="br_flag"
-                  className="flagImgs"
-                ></img>
-              </IconButton>
-              <IconButton onClick={() => handleChangeLanguage("en")}>
-                <img
-                  width={"80%"}
-                  height={"25px"}
-                  src={bandeirauk}
-                  alt="uk_flag"
-                  className="flagImgs"
-                ></img>
-              </IconButton>
-            </Box>
-          </Box>
-        </Box>
-      </Paper>
-      <Box
+      <Container
+        component="main"
         sx={{
-          background:
-            /* switchTheme === 'light' ? */ "linear-gradient(135deg,rgb(255,203,112) 0%,rgb(199,81,192) 50%,rgb(65,88,208) 100%)" /* : "#0d1117" */,
+          flexGrow: 1,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "2rem",
         }}
-        className="Middle"
       >
-        <Box
-          sx={{ width: "100%" }}
-          display={"flex"}
-          justifyContent={"center"}
-          gap={"50px"}
-        >
+        <Typography>
           <Box
-            sx={{ fontSize: "12px", marginLeft: "50px", marginTop: "50px" }}
+            sx={{ width: "100%" }}
             display={"flex"}
-            alignItems={"start"}
-            justifyContent={"start"}
+            justifyContent={"center"}
+            gap={"3%"}
           >
-            <Paper
-              elevation={24}
-              sx={{ padding: "8px", fontFamily: "Montserrat" }}
+            <Box
+              sx={{ fontSize: "12px", marginLeft: "50px", marginTop: "50px" }}
+              display={"flex"}
+              alignItems={"start"}
+              justifyContent={"start"}
             >
-              {" "}
-              {changeLanguage === "ptbr" ? (
-                <>Últimos 10 commits</>
-              ) : (
-                <>10 Last GitHub Commits</>
-              )}
-              <List>
-                {data.slice(0, 10).map((date, index) => {
-                  const parsedDate = parseISO(date);
-                  const formattedDate = format(parsedDate, "PPpp");
+              <Paper
+                elevation={24}
+                sx={{ padding: "8px", fontFamily: "Montserrat" }}
+              >
+                {" "}
+                {changeLanguage === "ptbr" ? (
+                  <>Últimos 10 commits</>
+                ) : (
+                  <>10 Last GitHub Commits</>
+                )}
+                <List>
+                  {data.slice(0, 10).map((date, index) => {
+                    const parsedDate = parseISO(date);
+                    const formattedDate = format(parsedDate, "PPpp");
 
-                  return (
-                    <ListItem sx={{ padding: "5px" }} key={index}>
-                      <CircleIcon
-                        sx={{
-                          color: "green",
-                          fontSize: "7px",
-                          marginRight: "10px",
-                        }}
-                      />
-                      {formattedDate}
-                    </ListItem>
-                  );
-                })}
-              </List>
-              <GitHubIcon />
-            </Paper>
-          </Box>
-          <Box
-            sx={{
-              maxHeight: "300px",
-              width: "1px",
-              backgroundColor: "black",
-              margin: "82px 15px",
-              opacity: '30%'
-            }}
-          ></Box>
-          {changeLanguage === "ptbr" ? (
-            <TabsPanelPt></TabsPanelPt>
-          ) : (
-            <TabsPanel></TabsPanel>
-          )}
-          <Box
-            sx={{
-              mt: "30px",
-              display: "flex",
-              flexDirection: "column",
-              justifyItems: "start",
-              alignItems: "start",
-              width: "10%",
-              gap: "30px",
-            }}
-          >
-            {/*             <IconButton
+                    return (
+                      <ListItem sx={{ padding: "5px" }} key={index}>
+                        <CircleIcon
+                          sx={{
+                            color: "green",
+                            fontSize: "7px",
+                            marginRight: "10px",
+                          }}
+                        />
+                        {formattedDate}
+                      </ListItem>
+                    );
+                  })}
+                </List>
+                <GitHubIcon />
+              </Paper>
+            </Box>
+            <Box
               sx={{
-                "&:hover": {
-                  backgroundColor: "#0d1117",
-                },
-                backgroundColor: "#0d1117",
-              }} onClick={() => handleChangeTheme('light')}
-            >
-              <LightModeIcon sx={{ color: "white" }}></LightModeIcon>
-            </IconButton>
-            <IconButton sx={{ backgroundColor: "white" }} onClick={() => handleChangeTheme('dark')}>
-              <DarkModeIcon sx={{ color: "#0d1117" }}></DarkModeIcon>
-            </IconButton> */}
+                maxHeight: "300px",
+                width: "1px",
+                backgroundColor: "black",
+                margin: "82px 15px",
+                opacity: "30%",
+              }}
+            ></Box>
+            {changeLanguage === "ptbr" ? (
+              <TabsPanelPt></TabsPanelPt>
+            ) : (
+              <TabsPanel></TabsPanel>
+            )}
+            <Box
+              sx={{
+                mt: "30px",
+                display: "flex",
+                flexDirection: "column",
+                justifyItems: "start",
+                alignItems: "start",
+                width: "10%",
+                gap: "30px",
+              }}
+            ></Box>
           </Box>
-        </Box>
-        <Box
-          sx={{
-            position: "fixed",
-            mt: "auto",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            maxHeight: "6vh",
-            minHeight: "6vh",
-            background:
-            'black' /* "#050C1B linear-gradient(270deg, #050C1B, #1A4055 15%, #1A4055 35%, #050C1B 50%)" */,
-            fontSize: "12px",
-            fontFamily: "Montserrat",
-            color: "white",
-            gap: "30px",
-            overflow: "hidden",
-            left: 0,
-            bottom: 0,
-            width: "100%",
-          }}
+        </Typography>
+      </Container>
+
+      {/* Footer */}
+
+      <Box
+        component="footer"
+        sx={{
+          py: 2,
+          px: 3,
+          mt: "auto",
+          backgroundColor: "black",
+        }}
+      >
+        <Typography
+          sx={{ fontFamily: "Montserrat", fontSize: "12px", color: "white" }}
         >
           {changeLanguage === "ptbr" ? (
-            <Box display={"flex"} gap={"8px"} alignItems={"center"}>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                gap: "1%",
+              }}
+            >
               <Box>©</Box>
               <Box>{actualYear}</Box>
               <Box>Davi Coelho — Todos os direitos reservados.</Box>
@@ -262,7 +210,13 @@ function App() {
               <Box>v1</Box>
             </Box>
           ) : (
-            <Box display={"flex"} gap={"8px"} alignItems={"center"}>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
               <Box>©</Box>
               <Box>{actualYear}</Box>
               <Box>Davi Coelho — All rights reserved.</Box>
@@ -280,7 +234,7 @@ function App() {
               <Box>v1</Box>
             </Box>
           )}
-        </Box>
+        </Typography>
       </Box>
     </Box>
   );
